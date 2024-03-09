223244d3ebe056f79474696f0fe1385dd952998b
loviscus-clon <kkiruk2929@naver.com>
2024-03-09 15:04:39 +0900

    column

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ async function clearColumns() {
   columnWrapEl.innerHTML = '';
 }
 
+// 딱 아래꺼만 가져가서 쓰고 이게 뭔지 제대로 숙지
+/**
+
+ * @returns {Promise<{ uri: string; title: string; thumbnail: string; content: string; date: number; }[]>}
+ */
 async function fetchColumns() {
   const r = await fetch('http://127.0.0.1:3000/api/column', {
     method: 'POST',
